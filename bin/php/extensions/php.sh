41cddd5611aa51f813312d81533f47a56208caca
zornshuai <zornshuai@foxmail.com>
2020-07-02 10:45:39 +0800

    将php扩展默认从Commands: build                  Build an Extension From C Source bundle                 Unpacks a Pecl Package channel-add            Add a Channel channel-alias          Specify an alias to a channel name channel-delete         Remove a Channel From the List channel-discover       Initialize a Channel from its server channel-info           Retrieve Information on a Channel channel-login          Connects and authenticates to remote channel server channel-logout         Logs out from the remote channel server channel-update         Update an Existing Channel clear-cache            Clear Web Services Cache config-create          Create a Default configuration file config-get             Show One Setting config-help            Show Information About Setting config-set             Change Setting config-show            Show All Settings convert                Convert a package.xml 1.0 to package.xml 2.0 format cvsdiff                Run a "cvs diff" for all files in a package cvstag                 Set CVS Release Tag download               Download Package download-all           Downloads each available package from the default channel info                   Display information about a package install                Install Package list                   List Installed Packages In The Default Channel list-all               List All Packages list-channels          List Available Channels list-files             List Files In Installed Package list-upgrades          List Available Upgrades login                  Connects and authenticates to remote server [Deprecated in favor of channel-login] logout                 Logs out from the remote server [Deprecated in favor of channel-logout] makerpm                Builds an RPM spec file from a PEAR package package                Build Package package-dependencies   Show package dependencies package-validate       Validate Package Consistency pickle                 Build PECL Package remote-info            Information About Remote Packages remote-list            List Remote Packages run-scripts            Run Post-Install Scripts bundled with a package run-tests              Run Regression Tests search                 Search remote package database shell-test             Shell Script Test sign                   Sign a package distribution file svntag                 Set SVN Release Tag uninstall              Un-install Package update-channels        Update the Channel List upgrade                Upgrade Package upgrade-all            Upgrade All Packages [Deprecated in favor of calling upgrade with no parameters] Usage: pecl [options] command [command-options] <parameters> Type "pecl help options" to list all options. Type "pecl help shortcuts" to list all command shortcuts. Type "pecl help version" or "pecl version" to list version information. Type "pecl help <command>" to get the help for the specified command.拉取

--- bin/php/extensions/php.sh
+++ bin/php/extensions/php.sh
@@ -11,6 +11,14 @@ echo "============================================"
 echo
 
 
+if [ -z "${EXTENSIONS##*,tideways_xhprof,*}" ]; then
+    echo "---------- Install tideways_xhprof ----------"
+    mkdir tideways_xhprof \
+    && tar -xf tideways-5.0.2.tgz -C tideways_xhprof --strip-components=1 \
+    && ( cd tideways_xhprof && phpize && ./configure && make ${MC} && make install ) \
+    && docker-php-ext-enable tideways_xhprof
+fi
+
 if [ -z "${EXTENSIONS##*,mcrypt,*}" ]; then
     echo "---------- mcrypt was REMOVED from PHP 7.2.0 ----------"
 fi
@@ -29,63 +37,52 @@ fi
 
 if [ -z "${EXTENSIONS##*,redis,*}" ]; then
     echo "---------- Install redis ----------"
-    mkdir redis \
-    && tar -xf redis-4.1.1.tgz -C redis --strip-components=1 \
-    && ( cd redis && phpize && ./configure && make ${MC} && make install ) \
-    && docker-php-ext-enable redis
+    apk add --no-cache
+    printf "\n" | pecl install redis
+    docker-php-ext-enable redis
 fi
 
 
 if [ -z "${EXTENSIONS##*,memcached,*}" ]; then
     echo "---------- Install memcached ----------"
-	apk add --no-cache libmemcached-dev zlib-dev
-    printf "\n" | pecl install memcached-3.1.3
+    apk add --no-cache
+    printf "\n" | pecl install memcached
     docker-php-ext-enable memcached
 fi
 
 
 if [ -z "${EXTENSIONS##*,xdebug,*}" ]; then
     echo "---------- Install xdebug ----------"
-    mkdir xdebug \
-    && tar -xf xdebug-2.6.1.tgz -C xdebug --strip-components=1 \
-    && ( cd xdebug && phpize && ./configure && make ${MC} && make install ) \
-    && docker-php-ext-enable xdebug
+    apk add --no-cache
+    printf "\n" | pecl install xdebug
+    docker-php-ext-enable xdebug
 fi
 
 
 if [ -z "${EXTENSIONS##*,swoole,*}" ]; then
     echo "---------- Install swoole ----------"
-    mkdir swoole \
-    && tar -xf swoole-4.2.1.tgz -C swoole --strip-components=1 \
-    && ( cd swoole && phpize && ./configure --enable-openssl && make ${MC} && make install ) \
-    && docker-php-ext-enable swoole
+    apk add --no-cache
+    printf "\n" | pecl install swoole
+    docker-php-ext-enable swoole
 fi
 
 if [ -z "${EXTENSIONS##*,pdo_sqlsrv,*}" ]; then
     echo "---------- Install pdo_sqlsrv ----------"
-	apk add --no-cache unixodbc-dev
+    apk add --no-cache
     pecl install pdo_sqlsrv
     docker-php-ext-enable pdo_sqlsrv
 fi
 
 if [ -z "${EXTENSIONS##*,sqlsrv,*}" ]; then
     echo "---------- Install sqlsrv ----------"
-	apk add --no-cache unixodbc-dev
+    apk add --no-cache
     printf "\n" | pecl install sqlsrv
     docker-php-ext-enable sqlsrv
 fi
 
 if [ -z "${EXTENSIONS##*,mongodb,*}" ]; then
     echo "---------- Install mongodb ----------"
-	apk add --no-cache unixodbc-dev
+    apk add --no-cache
     printf "\n" | pecl install mongodb
     docker-php-ext-enable mongodb
-fi
-
-if [ -z "${EXTENSIONS##*,tideways,*}" ]; then
-    echo "---------- Install tideways ----------"
-    mkdir tideways \
-    && tar -xf tideways-4.1.7.tar.gz -C tideways --strip-components=1 \
-    && ( cd tideways && phpize && ./configure && make ${MC} && make install ) \
-    && docker-php-ext-enable tideways
 fi
